95a788089189a70706100260ea2e349e1ac8fa92
reshetnikd <dmitry.reshetnik@icloud.com>
2021-05-27 18:12:04 +0300

    Refactor Code Base

--- FilesTree/Controllers/SpinnerViewController.swift
+++ FilesTree/Controllers/SpinnerViewController.swift
@@ -22,15 +22,4 @@ class SpinnerViewController: UIViewController {
         spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
